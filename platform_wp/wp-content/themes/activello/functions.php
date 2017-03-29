f95ff6e1151837c0ab19ac8701e38c8cbc7be026
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2017-03-29 23:30:12 +0200

    KNBER [change] widget posts slider

--- platform_wp/wp-content/themes/activello/functions.php
+++ platform_wp/wp-content/themes/activello/functions.php
@@ -322,3 +322,27 @@ if ( ! function_exists( 'activello_header_search_filter' ) ) {
 
 // Add welcome screen
 require get_template_directory() . '/inc/welcome-screen/welcome-page-setup.php';
+
+
+function add_featured_image_display_settings( $content, $post_id ) {
+	$field_id    = 'show_featured_image';
+	$field_value = esc_attr( get_post_meta( $post_id, $field_id, true ) );
+	$field_text  = esc_html__( 'Show in slider.', 'generatewp' );
+	$field_state = checked( $field_value, 1, false);
+
+	$field_label = sprintf(
+	    '<p><label for="%1$s"><input type="checkbox" name="%1$s" id="%1$s" value="%2$s" %3$s> %4$s</label></p>',
+	    $field_id, $field_value, $field_state, $field_text
+	);
+
+	return $content .= $field_label;
+}
+add_filter( 'admin_post_thumbnail_html', 'add_featured_image_display_settings', 10, 2 );
+
+function save_featured_image_display_settings( $post_ID, $post, $update ) {
+	$field_id    = 'show_featured_image';
+	$field_value = isset( $_REQUEST[ $field_id ] ) ? 1 : 0;
+
+	update_post_meta( $post_ID, $field_id, $field_value );
+}
+add_action( 'save_post', 'save_featured_image_display_settings', 10, 3 );
